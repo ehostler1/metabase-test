b08385fcfcdb7fb7bdaa10320a40ba6d24f98e87
Ethan <ethan@thirtytech.net>
2025-10-10 09:29:41 -0400

    data access and overwrite fixes

--- sidekick/backend/src/sidekick/common.clj
+++ sidekick/backend/src/sidekick/common.clj
@@ -7,9 +7,9 @@
 (defn- create-local-db
   "Given the Sidekick database parent, creates a new local database for the given local."
   [sidekick-db-id local]
+  (log/infof "Creating local database for Sidekick DB %s and local %s" sidekick-db-id local)
   (let [sidekick-db (t2/select-one :model/Database :id sidekick-db-id)
         {:keys [name details is_on_demand auto_run_queries cache_ttl creator_id is_full_sync]} sidekick-db]
-    (log/infof "Database info: %s" (pr-str sidekick-db))
     (first (t2/insert-returning-instances!
             :model/Database
             {:name         (str name " (" local ")")
@@ -26,11 +26,13 @@
   [sidekick-db local]
   (log/infof "Fetching local database for Sidekick DB %s and local %s" (u/the-id sidekick-db) local)
   (if (= (:engine sidekick-db) :sidekick)
-    (let [local-db-id (get-in sidekick-db [:details :local-databases local])]
+    (let [local-db-id (get-in sidekick-db [:details :local-databases (keyword local)])]
+      (log/infof "Sidekick database local databases: %s" (pr-str (get-in sidekick-db [:details :local-databases])))
+      (log/infof "Local database ID is %s" local-db-id)
       (if-let [local-db (if local-db-id (t2/select-one :model/Database :id local-db-id) nil)]
         local-db
         (let [local-db (create-local-db (:id sidekick-db) local)]
-          (log/info "This is a test")
-          (t2/update! :model/Database (u/the-id sidekick-db) {:details {:local-databases {local (u/the-id local-db)}}})
+          (log/infof "Created local database is: %s" (pr-str local-db))
+          (t2/update! :model/Database (u/the-id sidekick-db) {:details (assoc-in (:details sidekick-db) [:local-databases local] (u/the-id local-db))})
           local-db)))
     (throw (ex-info "Not a Sidekick database" {:status-code 400}))))
