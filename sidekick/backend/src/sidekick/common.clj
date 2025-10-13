ab3e1d4c27eceef2d48a4a0724d1ae799a06d5fc
Ethan <ethan@thirtytech.net>
2025-10-13 08:42:25 -0400

    local db sync fix

--- sidekick/backend/src/sidekick/common.clj
+++ sidekick/backend/src/sidekick/common.clj
@@ -1,5 +1,7 @@
 (ns sidekick.common
   (:require
+   [metabase.api.common :as api]
+   [metabase.events.core :as events]
    [metabase.util :as u]
    [metabase.util.log :as log]
    [toucan2.core :as t2]))
@@ -9,25 +11,27 @@
   [sidekick-db-id local]
   (log/infof "Creating local database for Sidekick DB %s and local %s" sidekick-db-id local)
   (let [sidekick-db (t2/select-one :model/Database :id sidekick-db-id)
-        {:keys [name details is_on_demand auto_run_queries cache_ttl creator_id is_full_sync]} sidekick-db]
-    (first (t2/insert-returning-instances!
-            :model/Database
-            {:name         (str name " (" local ")")
-             :engine       :sidekick
-             :details      (merge details {:local local :sidekick-parent-id sidekick-db-id :local-databases {}})
-             :auto_run_queries   auto_run_queries
-             :is_full_sync       is_full_sync
-             :is_on_demand       is_on_demand
-             :cache_ttl          cache_ttl
-             :creator_id         creator_id}))))
+        {:keys [name details]} sidekick-db]
+    (u/prog1 (t2/insert-returning-instance!
+              :model/Database
+              (merge
+               (dissoc sidekick-db :id :created_at :initial_sync_status :features :description :updated_at)
+               {:name         (str name " (" local ")")
+                :engine       :sidekick
+                :is_full_sync false
+                :details      (merge (dissoc details :local-databases :admin-user :admin-password) {:local local :sidekick-parent-id sidekick-db-id})}))
+             (events/publish-event! :event/database-create {:object <>
+                                                            :user-id api/*current-user-id*
+                                                            :details {:slug (:name <>)
+                                                                      :sidekick_db_name (:name sidekick-db)
+                                                                      :sidekick_db_id (:id sidekick-db)}}))))
 
 (defn sidekick-db->local-db
   "Given a Sidekick database and local, returns the the local database that should be used to connect."
   [sidekick-db local]
   (log/infof "Fetching local database for Sidekick DB %s and local %s" (u/the-id sidekick-db) local)
   (if (= (:engine sidekick-db) :sidekick)
-    (let [local-db-id (get-in sidekick-db [:details :local-databases (keyword local)])]
-      (log/infof "Sidekick database local databases: %s" (pr-str (get-in sidekick-db [:details :local-databases])))
+    (let [local-db-id (get-in sidekick-db [:details :local-databases (keyword local)])] 
       (log/infof "Local database ID is %s" local-db-id)
       (if-let [local-db (if local-db-id (t2/select-one :model/Database :id local-db-id) nil)]
         local-db
